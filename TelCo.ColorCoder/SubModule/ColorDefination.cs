7ab93238e4c491d095cc61e983529e0f7f2b8c69
kavyashukla18 <kavya.shukla@philips.com>
2020-08-25 15:46:38 +0530

    Modular_Programming

---
+++ TelCo.ColorCoder/SubModule/ColorDefination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TelCo.ColorCoder.SubModule
+{
+    class ColorDefination
+    {
+        public static readonly Color[] colorMapMajor;
+        public static readonly Color[] colorMapMinor;
+        static ColorDefination()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+    }
+}
---
+++ TelCo.ColorCoder/SubModule/ColorPair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TelCo.ColorCoder.SubModule
+{
+    class ColorPair
+    {
+        internal Color majorColor;
+        internal Color minorColor;
+        public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+        }
+    }
+}
