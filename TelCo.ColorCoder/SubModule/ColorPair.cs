81a626d511db3398349d7897d5f55c090524a7a0
kavyashukla18 <kavya.shukla@philips.com>
2020-08-25 16:22:30 +0530

    Modular

---
+++ TelCo.ColorCoder/SubModule/ColorPair.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Collections.Generic;
+using static TelCo.ColorCoder.SubModule.ColorDefination;
+
+namespace TelCo.ColorCoder.SubModule
+{
+    class ColorPair
+    {
+        internal Color majorColor;
+        internal Color minorColor;
+        public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+        }
+
+        public static ColorPair GetColorFromPairNumber(int pairNumber)
+        {
+            // The function supports only 1 based index. Pair numbers valid are from 1 to 25
+            int minorSize = colorMapMinor.Length;
+            int majorSize = colorMapMajor.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+            }
+            // Find index of major and minor color from pair number
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+            // Construct the return val from the arrays
+            ColorPair pair = new ColorPair()
+            {
+                majorColor = colorMapMajor[majorIndex],
+                minorColor = colorMapMinor[minorIndex]
+            };
+            // return the value
+            return pair;
+        }
+    }
+}
